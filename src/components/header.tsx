5390b4ea8851579a3e4af639fa47356f8bd1c1cb
vlad-blip <ezio2506@gmail.com>
2025-07-31 17:29:29 +0300

    + added light and dark mode

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,9 +1,11 @@
 import { Auth } from "./auth";
+import { ThemeToggle } from "./theme-toggle";
 
 export function Header() {
   return (
     <div className="flex justify-between px-10">
       <h6>webloom</h6>
+      <ThemeToggle />
       <Auth />
     </div>
   );
